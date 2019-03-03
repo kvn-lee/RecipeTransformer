5cf801701e2521fdfa6318c3bc67431589e658f7
Kevin Lee <kevin.son.lee@gmail.com>
2019-03-02 21:08:21 -0600

    Editing parser

--- parser.py
+++ parser.py
@@ -1,5 +1,48 @@
 import requests
+import pandas as pd
 from bs4 import BeautifulSoup
 
-page = requests.get("http://www.allrecipes.com/recipes")
-soup = BeautifulSoup(page.content, 'html.parser')
+
+def parseIngredients(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    dish = soup.find(id="recipe-main-content").get_text()
+    print("Parsing the ingredients necessary for " + dish)
+
+    page_list1 = soup.find(id="lst_ingredients_1")
+    page_list2 = soup.find(id="lst_ingredients_2")
+    check_list1 = page_list1.find_all(class_="checkList__line")
+    check_list2 = page_list2.find_all(class_="checkList__line")
+
+    items = []
+
+    for item1 in check_list1:
+        items.append(
+            str(item1.find(class_="recipe-ingred_txt added").get_text()))
+    for item2 in check_list2:
+        temp = item2.find(class_="recipe-ingred_txt added")
+        if temp:
+            items.append(
+                str(item2.find(class_="recipe-ingred_txt added").get_text()))
+
+    return items
+
+
+def parseDirections(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    dish = soup.find(id="recipe-main-content").get_text()
+    print("Parsing the directions necessary for " + dish)
+
+    pg_list = soup.find(class_="list-numbers recipe-directions__list")
+    dir_list = pg_list.find_all(class_="step")
+
+    steps = []
+
+    for step in dir_list:
+        steps.append(
+            str(step.find(class_="recipe-directions__list--item").get_text().rstrip()))
+
+    return steps
